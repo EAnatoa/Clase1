2c0ff31f451c979bb32bc13565833484828474b7
EAnatoa <stefy_nf92@hotmail.com>
2018-04-17 15:07:40 -0500

    variables

---
+++ 01-tipos-variables/tipos.js
@@ -0,0 +1,20 @@
+var nombre = "28";
+var edad = 28   // para enteros
+var fecha = new Date();
+var casado = true;  // true
+var casa = undefined;
+var arreglo = []
+
+console.log(casado?"verdadero":"Falso");
+
+var carro = null;
+
+  console.log(fecha);
+
+
+if (nombre === edad){
+    console.log("verdadero");
+
+}else{
+    console.log("falso");
+}

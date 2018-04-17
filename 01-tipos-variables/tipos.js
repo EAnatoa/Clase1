e69a63dd5cc769aa01de0b1b8490f40fa913d530
EAnatoa <stefy_nf92@hotmail.com>
2018-04-17 15:54:54 -0500

    final

--- 01-tipos-variables/tipos.js
+++ 01-tipos-variables/tipos.js
@@ -4,6 +4,21 @@ var fecha = new Date();
 var casado = true;  // true
 var casa = undefined;
 var arreglo = []
+var arregloNumero = [1,2,3]
+
+
+function sumarDosNumeros(numeroUno,numeroDos){
+    return numeroUno + numeroDos
+};
+
+var arreglo1 = ["hola",3,undefined,null,sumarDosNumeros(1,2)]
+
+console.log(arreglo1);
+
+for (var aux=0; aux < 10; aux++)
+{
+    console.log('Aux',aux)
+}
 
 console.log(casado?"verdadero":"Falso");
 
@@ -18,3 +33,81 @@ if (nombre === edad){
 }else{
     console.log("falso");
 }
+
+
+// para escribir un jason para objetos
+var usuario = {
+    "nombre":"Erika",
+    apellido:"Anatoa",
+    cedula:`1721473823`,};
+console.log("usuario: ", usuario);
+
+// para escribir un jason para objetos
+var usuario = {
+    "nombre": "Erika",
+    apellido: "Anatoa",
+    cedula: '1721473823' ,
+    imprimirEnConsola: sumarDosNumerosVersionDos
+
+};
+//funcion
+function sumarDosNumeros(numeroUno,numeroDos){
+    return numeroUno + numeroDos
+};
+
+console.log (sumarDosNumeros(1,2));
+
+
+
+
+//
+var sumarDosNumerosVersionDos =  function sumarDosNumeros(numeroUno,numeroDos){
+    return numeroUno + numeroDos
+};
+
+console.log (sumarDosNumeros(1,2));
+console.log(usuario.cedula);
+console.log(usuario.nombre);
+console.log(usuario.apellido);
+console.log(usuario.imprimirEnConsola(1,2));
+
+
+
+
+// para escribir un jason para objetos
+var usuario = {
+    "nombre": "Erika",
+    apellido: "Anatoa",
+    cedula: '1721473823' ,
+    imprimirEnConsola: sumarDosNumerosVersionDos
+
+};
+
+console.log(usuario.cedula);
+console.log(usuario.nombre);
+console.log(usuario.apellido);
+console.log(usuario.imprimirEnConsola(1,2));
+
+//solo imprimos el
+
+
+
+    //funcion
+    function sumarDosNumeros(numeroUno,numeroDos){
+        return numeroUno + numeroDos
+};
+
+console.log (sumarDosNumeros(1,2));
+
+
+
+
+//
+    var sumarDosNumerosVersionDos =  function sumarDosNumeros(numeroUno,numeroDos){
+        return numeroUno + numeroDos
+    };
+
+console.log (sumarDosNumeros(1,2));
+
+
+
